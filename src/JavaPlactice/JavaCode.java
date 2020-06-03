efc61d0b462210b34dd1dfa66788e62fb4941242
kagawa <tekken77marduk>
2020-06-03 12:40:02 +0900

    try~catch

--- src/JavaPlactice/JavaCode.java
+++ src/JavaPlactice/JavaCode.java
@@ -15,8 +15,14 @@ public class JavaCode {
 //        }
 		 int a = 4;
 		 int b = 0;
+		 try {
 		 int c = a / b;
 		 System.out.println("cの値は" + c);
-		 System.out.println("処理を正常に終了します");
+		 }
+		catch(ArithmeticException e) {
+		 System.out.println("例外をキャッチしました");
+		 System.out.println(e);
+		 }
+		 System.out.println("プログラムを終了します");
     }
 }
